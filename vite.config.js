17f861d54ce1307e4cc499cce6d27738432e3a6c
TalesSMag <127444509+TalesSMag@users.noreply.github.com>
2025-11-03 17:40:49 -0300

    Refactor Vite config for path alias resolution
    Updated path resolution for alias '@' in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // ✅ Import via ESM
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve('./src'), // @ aponta para a pasta src
     },
-  }
-})
+  },
+});
+
