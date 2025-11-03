b79880a97757d38590bb58902909e3d8694df9ed
TalesSMag <127444509+TalesSMag@users.noreply.github.com>
2025-11-03 18:23:47 -0300

    Change process.env to process.meta.env for API_URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3003";
+const API_URL = process.meta.env.NEXT_PUBLIC_API_URL || "http://localhost:3003";
 
 export default API_URL;
